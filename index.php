bebbdf00edb3ec7f265c3d09838286f7fe1be136
Tavi03 <taviurdu@yahoo.com>
2024-05-16 20:27:39 +0300

    ceva scurt

--- index.php
+++ index.php
@@ -3,49 +3,110 @@ include "inc/header.php";
 ?>
 
 <body>
-    <style>
-        body {
-            margin: 0;
-        }
-    </style>
     <?php
     include "inc/navbar.php";
     ?>
 
     <!-- content -->
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate, mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aperiam numquam odit ad voluptas laudantium
+        animi dolores eius excepturi fuga quidem consectetur eveniet iusto, officiis debitis, soluta cum. Voluptate,
+        mollitia.</p>
 
     <?php
     include "inc/footer.php";
