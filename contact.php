c74dfc231daa5b8c35ce213ef91152e8e0dac88c
Tavi03 <taviurdu@yahoo.com>
2024-05-22 19:12:35 +0300

    contact page started

---
+++ contact.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "header.php"; ?>
+    <link rel="stylesheet" href="styles/contact.css">
+    <title>Real Madrid C.F. | Contact</title>
+</head>
+
+<body>
+    <?php include "navbar/navbar.php"; ?>
+
+    <div class="header">
+        <h1>
+            Contact
+        </h1>
+        <img src="images/contact/real madrid logo gri.svg" alt="real madrid logo">
+    </div>
+
+    <div class="content">
+        <h2>Real Madrid</h2>
+        <p>Telephone +34 91 398 43 00</p>
+        
+    </div>
+
+    <?php include "footer/footer.php"; ?>
+</body>
+
+</html>
--- header.php
+++ header.php
@@ -5,4 +5,5 @@
     <link rel="stylesheet" href="navbar/navstyle.css">
     <link rel="stylesheet" href="footer/footerstyle.css">
     <link href="https://db.onlinewebfonts.com/c/8a3314077ac9ebc46b4620ff2e486726?family=RM+Neue+Bold" rel="stylesheet">
+    <link href="https://db.onlinewebfonts.com/c/01c1a10005e69313813f6423c24fa730?family=RM+Neue+Light" rel="stylesheet">
 </head>
