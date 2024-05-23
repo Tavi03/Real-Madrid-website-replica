ed61eae7bec5978722414aa7684aeb8ab4acff3f
Tavi03 <taviurdu@yahoo.com>
2024-05-23 20:40:16 +0300

    sectii part progress

--- sectii.php
+++ sectii.php
@@ -10,12 +10,55 @@
 <body>
     <?php include "navbar/navbar.php"; ?>
 
-    <!-- Content -->
+    <!-- Header -->
     <div class="header">
         <h1>
             Sectii
         </h1>
         <img src="images/contact/real madrid logo gri.svg" alt="real madrid logo">
+    </div>
+
+    <!-- Content -->
+    <div class="main-page">
+        <div class="main-article">
+            <img class="main-image" src="images/sectii/football team.jpg" alt="football team">
+            <h1>Fotbal</h1>
+        </div>
+
+        <div class="secondary-article">
+            <h2>Campionii Fotbalului European</h2>
+            <img class="secondary-article-image" src="images/sectii/football team4.jpg"
+                alt="champions league starting 11">
+        </div>
+
+        <div class="secondary-article">
+            <h2>Real Madrid, recunoscută de Villareal drept campioană înaintea meciului de pe Estadio de la Cerámica
+            </h2>
+            <img src="images/home/villarreal real madrid.jpg" alt="villarreal - real madrid"
+                class="secondary-article-image">
+        </div>
+    </div>
+
+    <div class="separator"></div>
+
+    <div class="main-page">
+        <div class="main-article">
+            <img class="main-image" src="images/sectii/basketball team.jpg" alt="football team">
+            <h1>Campionii Basketului European</h1>
+        </div>
+
+        <div class="secondary-article">
+            <h2>Hai să susținem echipa în finala Champions League de pe Wembley!</h2>
+            <img class="secondary-article-image" src="images/sectii/football team4.jpg"
+                alt="champions league starting 11">
+        </div>
+
+        <div class="secondary-article">
+            <h2>Real Madrid, recunoscută de Villareal drept campioană înaintea meciului de pe Estadio de la Cerámica
+            </h2>
+            <img src="images/home/villarreal real madrid.jpg" alt="villarreal - real madrid"
+                class="secondary-article-image">
+        </div>
     </div>
 
     <?php include "footer/footer.php"; ?>
