33bfa8a921e6558b26b2baf172b0a89757d4a5aa
Tavi03 <taviurdu@yahoo.com>
2024-05-23 10:51:17 +0300

    contact page done

--- contact.php
+++ contact.php
@@ -20,7 +20,73 @@
     <div class="content">
         <h2>Real Madrid</h2>
         <p>Telephone +34 91 398 43 00</p>
-        
+        <p>Fax +34 91 398 43 82</p>
+        <p>Postal Adress</p>
+        <p>
+            SANTIAGO BERNABÉU STADIUM
+            <br>
+            Avenida Concha Espina, Nº 1 - Door 44
+            <br>
+            28036 Madrid
+            <br><br>
+            CIUDAD REAL MADRID
+            <br>
+            Camino Sintra s/n
+            <br>
+            28055 Madrid
+        </p>
+        <h2>Customer Service</h2>
+        <p>The customer service office is open from Monday to Friday.</p>
+        <p>REAL MADRID PUBLIC SERVICE LINE +34 91 121 00 90</p>
+        <h3>E-Mail</h3>
+        <p>
+            <a href="mailto:atencionpublico@corp.realmadrid.com">atencionpublico@corp.realmadrid.com</a>
+        </p>
+        <h2>Helpdesk for Club Members</h2>
+        <p>TELEPHONE +34 91 344 54 45</p>
+        <p>Club members must prove their identity with their member's number and their PIN number. Once these numbers
+            have been validated, they can use a personalized service.</p>
+        <p>Real Madrid Line +34 91 121 00 90</p>
+        <h3>E-Mail</h3>
+        <p>
+            <a href="mailto:oas@corp.realmadrid.com">oas@corp.realmadrid.com</a>
+        </p>
+        <h2>Fan Club Department</h2>
+        <p>Those interested must write a letter to the Fan Club Department at Real Madrid expressing their wish to
+            create a new fan club.</p>
+        <p>Fax +34 91 398 43 36</p>
+        <h3>E-Mail</h3>
+        <p>
+            <a href="mailto:oap@corp.realmadrid.es">oap@corp.realmadrid.es</a>
+        </p>
+        <h2>PRESS</h2>
+        <p>TELEPHONE +34 91 398 43 00</p>
+        <h2>TOUR BERNABÉU</h2>
+        <p>TELEPHONE +34 91 398 43 70</p>
+        <h3>E-Mail</h3>
+        <a href="mailto:tour@corp.realmadrid.com">tour@corp.realmadrid.com</a>
+        <h2>&nbsp;</h2>
+        <h2>Madridista Line</h2>
+        <p>TELEPHONE +34 91 374 24 00</p>
+        <p>Call Centre for Madridistas card holders. Attention of requests, queries and incidents resolution,
+            modification of personal data and information about the exclusive Madridistas advantages due to the program
+            of loyalty Madridista. Schedule From Monday to Sunday: from 9 to 21 hours, uninterrupted, every day of the
+            year.</p>
+        <h3>E-mail</h3>
+        <p>
+            <a href="mailto:madridista.cm@corp.realmadrid.com">madridista.cm@corp.realmadrid.com</a>
+        </p>
+        <h2>&nbsp;</h2>
+        <h2>Real Madrid App</h2>
+        <h3>E-Mail</h3>
+        <p>
+            <a href="mailto:product@realmadrid.es">product@realmadrid.es</a>
+        </p>
+        <h2>RM Play</h2>
+        <h3>E-Mail</h3>
+        <p>
+            <a href="mailto:madridistas@corp.realmadrid.com">madridistas@corp.realmadrid.com</a>
+        </p>
     </div>
 
     <?php include "footer/footer.php"; ?>
---
+++ sectii.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "header.php" ?>
+    <link rel="stylesheet" href="styles/sectiistyle.css">
+    <title>Real Madrid C.F. | Sectii</title>
+</head>
+
+<body>
+    <?php include "navbar/navbar.php"; ?>
+
+    <!-- Content -->
+    <div class="header">
+        <h1>
+            Sectii
+        </h1>
+        <img src="images/contact/real madrid logo gri.svg" alt="real madrid logo">
+    </div>
+
+    <?php include "footer/footer.php"; ?>
+</body>
+</html>
